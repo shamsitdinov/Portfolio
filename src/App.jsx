53acb20e581c568ac381375066b1d096f7b4001f
Nodirbek <shamsiddinovnodirbek469@gmail.com>
2025-04-03 20:39:54 +0500

    an

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black text-white'>
+      <h1>hello js</h1>
+      <button>send</button>
+      <p>hello</p>
+
+    </div>
+  )
+}
+
+export default App
